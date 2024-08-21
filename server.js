462a59d56ddd483a68502bb42f30e01f88584bf2
Pradipta Banerjee <banerjeepradipta08@gmail.com>
2024-08-22 00:08:03 +0530

    new

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("*",(req,res)=>{
 })
 
 const PORT=8000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
